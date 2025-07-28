69a65a17d95929c92634a09db659dc624a109f7f
Michael <hello@kanso.supply>
2025-07-28 20:32:08 +0200

    fixed things

--- app/page.tsx
+++ app/page.tsx
@@ -44,13 +44,13 @@ export default function Home() {
   );
 
   return (
-    <main ref={container} className="w-full overflow-visible">
-      <section className="h-screen overflow-hidden flex flex-col sm:pt-36 pt-24 pb-8 px-5 sm:px-10 sm:gap-32 gap-4 bg-[#F1FF00]">
+    <main ref={container}>
+      <section className="h-screen overflow-hidden flex flex-col px-5 sm:px-10 sm:gap-32 gap-12 sm:py-36 py-24 sm:pb-16 bg-electric">
         {/* Top section with grid layout - takes available space and grows */}
         <div className="sm:grid sm:grid-cols-4 flex flex-col items-start gap-8 h-full">
           <h1 className="col-span-1">Minimal works.</h1>
-          <div className="sm:col-start-3 flex h-full">
-            <div className="relative flex h-full aspect-portrait">
+          <div className="sm:col-start-3 flex h-full w-full sm:w-auto">
+            <div className="relative flex sm:aspect-portrait h-full w-full sm:w-auto">
               <Image
                 ref={imageRef}
                 src="/portrait.jpeg"
@@ -64,8 +64,8 @@ export default function Home() {
 
         {/* Bottom section with grid layout - only takes content height */}
         <div className="sm:grid sm:grid-cols-2 flex flex-col items-start gap-8">
-          <div className="flex flex-row gap-2 pt-[16px]">
-            <div className="w-[16px] h-[16px] bg-foreground" />
+          <div className="flex flex-row items-center gap-2">
+            <div className="w-[12px] h-[12px] bg-foreground" />
             <h3>About me</h3>
           </div>
           <h2 ref={h2Ref}>
@@ -74,19 +74,27 @@ export default function Home() {
           </h2>
         </div>
       </section>
-
-      <section className="h-screen overflow-hidden flex flex-col justify-between pt-36 pb-8 px-5 sm:px-10">
+      <section className="overflow-hidden flex flex-col gap-16 py-40 px-5 sm:px-10">
         {/* Top section with grid layout */}
-        <div className="grid sm:grid-cols-2 grid-cols-1 items-center gap-4">
+        <div className="sm:grid sm:grid-cols-2 flex flex-col items-center gap-4">
           <h2>Selected works.</h2>
+          <h2>22/25</h2>
         </div>
+
         {/* Bottom section with grid layout */}
-        <div className="grid grid-cols-2 items-start gap-4">
-          <div className="flex flex-row gap-2">
+        <div className="grid grid-cols-2 items-end gap-4">
+          <div className="flex flex-row gap-2 items-center">
             <div className="w-[16px] h-[16px] bg-foreground" />
-            <h3>In progress.</h3>
+            <h2>01</h2>
           </div>
-          <Image src="/connor.jpeg" width={500} height={500} alt="hero image" />
+          <div className="flex relative aspect-16/10 h-auto w-full gap-2">
+            <Image
+              src="/connor.jpeg"
+              fill
+              className="object-cover"
+              alt="hero image"
+            />
+          </div>
         </div>
       </section>
     </main>
