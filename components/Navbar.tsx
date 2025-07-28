8b3e28e11d99a157d8380cf1a08e8905cf1e4ffd
Michael <hello@kanso.supply>
2025-07-28 13:29:29 +0200

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <div className="grid grid-cols-2 px-5 sm:px-10 items-center bg-[#f4f5f5] fixed top-0 left-0 right-0 h-18 z-10">
+      <h3>MNML</h3>
+      <h3>Info</h3>
+    </div>
+  );
+}
